ae120ad80dd6f0d129c68fe7403b28db3a792bdb
Abhijeet Muneshwar <abhijeet.muneshwar@quikr.com>
2016-06-20 08:06:01 +0530

    Solved Search -> pairs

--- Algorithm/Search/Pairs/index.php
+++ Algorithm/Search/Pairs/index.php
@@ -7,6 +7,24 @@
  * https://www.hackerrank.com/challenges/pairs
  */
 
+fscanf(STDIN, "%d %d", $len, $k);
+$arr = array_map('intval', explode(" ", trim(fgets(STDIN))));
+sort($arr);
+$i = 0;
+$j = 1;
+$count = 0;
+while ($j < $len) {
+    $diff = $arr[$j] - $arr[$i];
+    if ($diff === $k) {
+        $count++;
+        $j++;
+    } elseif ($diff > $k) {
+        $i++;
+    } else {
+        $j++;
+    }
+}
 
+echo $count . PHP_EOL;
 
 ?>
