0955a3ecd401605cefb07157489a22ab3ce7384b
Abhijeet Muneshwar <abhijeet.muneshwar@quikr.com>
2016-07-10 20:48:22 +0530

    Solved crab graph using maximum flow algorithm

---
+++ Algorithm/GraphTheory/CrabGraphs/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhijeet
+ * Date: 09/07/16
+ * Time: 5:19 PM
+ * https://www.hackerrank.com/challenges/crab-graphs
+ */
+fscanf(STDIN, "%d", $cases);
+while($cases--) {
+    fscanf(STDIN, "%d %d %d", $vertices, $t, $edges);
+    
+}
+?>
